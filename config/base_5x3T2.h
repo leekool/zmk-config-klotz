73f1262f0345a58096d6bdf3253bafef4663111c
leekool <lee@imre.al>
2023-10-23 22:17:45 +1100

    basic copy of crkbd map

--- config/base_5x3T2.h
+++ config/base_5x3T2.h
@@ -10,10 +10,10 @@
 #include <dt-bindings/zmk/outputs.h>
 
 #define APT 0
-#define NUM 1
-#define NAV 2
-#define SYM 3
-#define FUN 4
+#define NAV 1
+#define NUM 2
+#define FUN 3
+#define SYM 4
 #define BTC 5
 
 // &lt {
@@ -38,7 +38,7 @@
 
 #define COMBO(NAME, BINDINGS, KEYPOS) \
 combo_##NAME { \
-    timeout-ms = <20>; \
+    timeout-ms = <120>; \
     bindings = <BINDINGS>; \
     key-positions = <KEYPOS>; \
 };
@@ -46,31 +46,34 @@ combo_##NAME { \
 / {
     combos {
         compatible = "zmk,combos";
+        COMBO(delete, &kp DEL, 30 31)
+        COMBO(escape, &kp ESC, 32 33)
+        COMBO(grave, &kp GRAVE, 8 9)
 /* usually on base layer */
-        COMBO(q, &kp Q, 2 3)
-        COMBO(z, &kp Z, 21 22)
-        COMBO(v, &kp V, 1 2)
-        COMBO(sch, &macro_sch, 1 3)
-        COMBO(slash, &kp SLASH, 27 28)
-        COMBO(dash, &kp MINUS, 26 27)
-        COMBO(enter, &kp ENTER, 26 27 28)
-        COMBO(esc, &kp ESC, 22 23)
+        // COMBO(q, &kp Q, 2 3)
+        // COMBO(z, &kp Z, 21 22)
+        // COMBO(v, &kp V, 1 2)
+        // COMBO(sch, &macro_sch, 1 3)
+        // COMBO(slash, &kp SLASH, 27 28)
+        // COMBO(dash, &kp MINUS, 26 27)
+        // COMBO(enter, &kp ENTER, 26 27 28)
+        // COMBO(esc, &kp ESC, 22 23)
 /* parentheticals */
-        COMBO(lbrc, &kp LBRC, 1 11)
-        COMBO(lbkt, &kp LBKT, 2 12)
-        COMBO(lpar, &kp LPAR, 3 13)
-        COMBO(lt, &kp LT, 4 14)
-        COMBO(gt, &kp GT, 5 15)
-        COMBO(rpar, &kp RPAR, 6 16)
-        COMBO(rbkt, &kp RBKT, 7 17)
-        COMBO(rbrc, &kp RBRC, 8 18)
+        // COMBO(lbrc, &kp LBRC, 1 11)
+        // COMBO(lbkt, &kp LBKT, 2 12)
+        // COMBO(lpar, &kp LPAR, 3 13)
+        // COMBO(lt, &kp LT, 4 14)
+        // COMBO(gt, &kp GT, 5 15)
+        // COMBO(rpar, &kp RPAR, 6 16)
+        // COMBO(rbkt, &kp RBKT, 7 17)
+        // COMBO(rbrc, &kp RBRC, 8 18)
 /* caps */
-        COMBO(caps, &caps_word, 13 16)
-        COMBO(capslock, &kp CAPSLOCK, 1 8)
+        // COMBO(caps, &caps_word, 13 16)
+        // COMBO(capslock, &kp CAPSLOCK, 1 8)
 /* deletion */
-        COMBO(bspc, &kp BSPC, 6 7)
-        COMBO(del, &kp DEL, 7 8)
-        COMBO(delword, &kp LC(BSPC), 6 8)
+        // COMBO(bspc, &kp BSPC, 6 7)
+        // COMBO(del, &kp DEL, 7 8)
+        // COMBO(delword, &kp LC(BSPC), 6 8)
     };
 };
 
@@ -116,18 +119,27 @@ combo_##NAME { \
             // hold-trigger-key-positions = <0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23>;
         };
 
-        dotcol: dot_colon {
+        lpar_shft: lpar_shft {
             compatible = "zmk,behavior-mod-morph";
-            label = "DOT_COL";
+            label = "LPAR_SHFT";
             #binding-cells = <0>;
-            bindings = <&kp DOT>, <&kp COLON>;
+            bindings = <&kp LPAR>, <&kp RPAR>;
             mods = <(MOD_LSFT|MOD_RSFT)>;
         };
-        comsem: comma_semicolon {
+
+        lbkt_shft: lbkt_shft {
             compatible = "zmk,behavior-mod-morph";
-            label = "COM_SEM";
+            label = "LBKT_SHFT";
             #binding-cells = <0>;
-            bindings = <&kp COMMA>, <&kp SEMI>;
+            bindings = <&kp LBKT>, <&kp RBKT>;
+            mods = <(MOD_LSFT|MOD_RSFT)>;
+        };
+
+        lbrc_shft: lbrc_shft {
+            compatible = "zmk,behavior-mod-morph";
+            label = "LBRC_SHFT";
+            #binding-cells = <0>;
+            bindings = <&kp LBRC>, <&kp RBRC>;
             mods = <(MOD_LSFT|MOD_RSFT)>;
         };
     };
