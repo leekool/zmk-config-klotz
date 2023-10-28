e07051e76e201984df0a56f6f161ede2c4861f5e
leekool <lee@imre.al>
2023-10-28 16:33:16 +1100

    cs2

--- config/base_5x3T2.h
+++ config/base_5x3T2.h
@@ -9,12 +9,12 @@
 #include <dt-bindings/zmk/bt.h>
 #include <dt-bindings/zmk/outputs.h>
 
-#define APT 0
+#define LEE 0
 #define NAV 1
 #define NUM 2
 #define FUN 3
 #define BTC 4
-#define SYM 5
+#define CS2 5
 
 &caps_word {
     continue-list = <UNDERSCORE MINUS BSPC LSHFT RSHFT>;
@@ -32,31 +32,34 @@
 };
 
 
-#define COMBO(NAME, BINDINGS, KEYPOS) \
+#define COMBO(NAME, MS, BINDINGS, KEYPOS, LAYERS) \
 combo_##NAME { \
-    timeout-ms = <90>; \
+    timeout-ms = <MS>; \
     bindings = <BINDINGS>; \
     key-positions = <KEYPOS>; \
+    layers = <LAYERS>; \
 };
 
 / {
     combos {
         compatible = "zmk,combos";
-        COMBO(delete, &kp DEL, 30 31)
-        COMBO(fun_esc, &lt FUN ESC, 32 33)
-        COMBO(grave, &kp GRAVE, 8 9)
-        COMBO(semi, &kp SEMI, 15 25)
-        COMBO(dollar, &kp DOLLAR, 5 15)
-        COMBO(equal, &kp EQUAL, 6 16)
-        COMBO(escape, &kp ESC, 0 10)
-        COMBO(lpar, &lpar_shft, 4 14)
-        COMBO(rbkt, &lbrc_shft, 14 24)
-        // COMBO(alt_tab, &kp LA(TAB), 18 33)
-        COMBO(f11, &kp F11, 22 30)
-        COMBO(f12, &kp F12, 23 30)
-        // COMBO(del_word, &kp LC(BSPC), 12 30)
-        COMBO(const, &macro_const, 1 3)
-        COMBO(caps, &caps_word, 20 29)
+        COMBO(delete, 90, &kp DEL, 30 31, -1)
+        COMBO(fun_esc, 90, &lt FUN ESC, 32 33, -1)
+        COMBO(grave, 90, &kp GRAVE, 8 9, -1)
+        COMBO(semi, 90, &kp SEMI, 15 25, -1)
+        COMBO(dollar, 90, &kp DOLLAR, 5 15, -1)
+        COMBO(equal, 90, &kp EQUAL, 6 16, -1)
+        COMBO(escape, 50, &kp ESC, 0 1, -1)
+        COMBO(lpar, 90, &lpar_shft, 4 14, 0 1 2 3 4)
+        COMBO(rbkt, 90, &lbrc_shft, 14 24, 0 1 2 3 4)
+        COMBO(f11, 90, &kp F11, 22 30, -1)
+        COMBO(f12, 90, &kp F12, 23 30, -1)
+        COMBO(const, 90, &macro_const, 1 3, -1)
+        COMBO(caps, 90, &caps_word, 20 29, -1)
+
+        // CS2
+        COMBO(cs_n1, 90, &kp N1, 4 14, 5)
+        COMBO(cs_n2, 90, &kp N2, 14 24, 5)
     };
 };
 
